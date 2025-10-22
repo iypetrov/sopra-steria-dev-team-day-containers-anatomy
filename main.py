10eba40830b909d6cf6fa3a6e1ee0d6d46d2a40b
iypetrov <ilia.yavorov.petrov@gmail.com>
2025-10-22 15:09:53 +0000

    feat: add the setup with run function

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("hello world")
-print("hello world")
+import os
+import sys
+import subprocess
+
+def run():
+    cmd = sys.argv[2:]
+    print(cmd)
+    subprocess.run(cmd, stdin=sys.stdin, stdout=sys.stdout, stderr=sys.stderr)
+
+# docker         run image <cmd> <params>
+# python main.py run       <cmd> <params>
+def main():
+    if len(sys.argv) < 2:
+        sys.exit(1)
+
+    if sys.argv[1] == "run":
+        run()
+
+if __name__ == "__main__":
+    main()
